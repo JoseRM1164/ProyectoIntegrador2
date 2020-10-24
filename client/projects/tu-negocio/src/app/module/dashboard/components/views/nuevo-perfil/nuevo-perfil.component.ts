ce2e85ddd522f4186b7f06cf42de46164f962b5e
Jose Javier Tlacuilo Fuentes <A01420128@itesm.mx>
2020-10-24 11:15:27 -0500

    Agrega nuevo perfil en perfil/nuevo con su Form

---
+++ client/projects/tu-negocio/src/app/module/dashboard/components/views/nuevo-perfil/nuevo-perfil.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators, FormArray } from '@angular/forms';
+
+@Component({
+  selector: 'app-nuevo-perfil',
+  templateUrl: './nuevo-perfil.component.html',
+  styleUrls: ['./nuevo-perfil.component.scss']
+})
+export class NuevoPerfilComponent implements OnInit {
+  modeloPerfil = this.formBuild.group({
+      nombrePerfil: ['', Validators.required],
+      apellidosPerfil: ['', [Validators.required]],
+      correoPerfil: ['', [Validators.required, Validators.email]],
+      passwordPerfil: ['', Validators.required, Validators.minLength(8)],
+      telefonoPerfil: [''],
+      adminCheck: [''],
+      lecturaCheck: [''],
+      escrituraCheck: [''],
+      borradoCheck: ['']
+  });
+
+
+    constructor(private formBuild: FormBuilder) { }
+
+  ngOnInit(): void {
+  }
+
+  enviar() {}
+
+}
+
