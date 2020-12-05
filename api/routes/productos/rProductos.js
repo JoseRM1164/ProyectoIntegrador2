53495419d7edc0a73e2268251fde53d44b10de8f
Alejandro López Martínez <alejandrolm14@gmail.com>
2020-12-05 03:03:47 -0600

    hotfix lenguaje

--- api/routes/productos/rProductos.js
+++ api/routes/productos/rProductos.js
@@ -5,7 +5,6 @@ let Productos = require('../../models/productos');
 router.get('/', async (req, res, next) => {
 	const productos = await Productos.find(
 		{'invenID': req.query.invenID},
-		{'lang': req.query.lang}
 	);
 	
 	res.json(productos);
